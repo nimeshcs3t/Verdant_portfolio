11d96f59d2e9333b0b2bc1fa747a37b8a0275e58
nimeshcs3t <dr.nimeshcshah@gmail.com>
2026-02-28 18:43:10 -0500

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,16 @@
 const { getDefaultConfig } = require('expo/metro-config');
+
 const config = getDefaultConfig(__dirname);
-config.resolver.platforms = ['ios', 'android', 'native', 'web'];
+
+// Ensure we are not forcing static rendering if it's being stubborn
+config.transformer = {
+  ...config.transformer,
+  getTransformOptions: async () => ({
+    transform: {
+      experimentalImportSupport: false,
+      inlineRequires: true,
+    },
+  }),
+};
+
 module.exports = config;
